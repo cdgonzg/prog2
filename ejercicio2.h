08bf357dfc7ad705be1fa9cd83a2facc4d7076d0
Daniel González <cgonzalg@seminario.edu.uy>
2025-09-08 13:39:32 -0300

    ejercicio 1 completo

---
+++ ejercicio2.h
@@ -0,0 +1,4 @@
+#ifndef EJERCICIO2_H
+#define EJERCICIO2_H
+
+#endif
--- practico_5/ejercicio1.h
+++ practico_5/ejercicio1.h
@@ -2,11 +2,20 @@
 #define EJERCICIO1_H
 
 #include "boolean.h"
+#include "fecha.h"
+#include "string.h"
 
 //definicion de tipo de datos
 
 //ejercicio 1.a
 
+/***************************************************************************************/
+/*
+    En todos los casos podemos hacer un arreglo con tope para almacenar
+    los viajes en el primer caso
+    las reuniones o eventos en el segundo caso
+    y las arañas en el tercer caso
+*/
 //tipo de destinos nacionales posibles
 typedef enum{Colonia, San_Jose, Florida, Canelones, Brasil, Argentina, Paraguay} destino;
 
@@ -32,9 +41,62 @@ typedef struct
     } tipo_viaje;
 } viaje;
 
+/***************************************************************************************/
+
 //ejercicio 1.b
+
+/***************************************************************************************/
 typedef enum {Economia, Politica, Informatica, Tecnologia} temas_reunion;
 
+typedef struct
+{
+    String persona;
+    int hora;
+    temas_reunion tema;
+} reunion;
+
+typedef struct
+{
+    String pais;
+    temas_reunion tema;
+} evento;
+typedef struct
+{
+    fecha dia;
+    boolean es_reunion; //TRUE si es reunion, FALSE si es evento
+    union {
+        reunion reun;
+        evento even;
+    } tipo_encuentro;
+} agenda;
+/***************************************************************************************/
+
+//ejercicio 1.c
+/***************************************************************************************/
+typedef struct {
+    boolean come_ratones;
+} tarantula;
+
+typedef struct {
+    float nivel_veneno;
+    float peso;
+} viudas;
+
+typedef struct {
+    boolean urticante;
+    fecha fecha_hibernacion;
+} peludas;
+
+typedef struct{
+    String nombre;
+    int cant_patas;
+    union {
+        tarantula tara;
+        viudas viuda;
+        peludas peluda;
+    } tipo_arania;
+} arania;
+/***************************************************************************************/
 
 
 #endif
